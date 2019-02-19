3fb210d42c5d8e1fc7d6a32c808f46e880850f01
Christian Budtz <chrbudtz@gmail.com>
2019-02-19 11:47:11 +0100

    2nd commit

--- src/main/java/dal/UserDAOImpls134000.java
+++ src/main/java/dal/UserDAOImpls134000.java
@@ -2,17 +2,32 @@ package dal;
 
 import dto.UserDTO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 //TODO Rename class
 public class UserDAOImpls134000 implements IUserDAO {
     //TODO Make a connection to the database
+    private Connection createConnection() throws DALException {
+        try {
+            return DriverManager.getConnection("jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/chbu?"
+                    + "user=chbu&password=4thVbCaMOxnXi3aJ4");
+        } catch (SQLException e) {
+            throw new DALException(e.getMessage());
+        }
+    }
 
     @Override
     public UserDTO getUser(int userId) throws DALException {
         //TODO Implement this
+        Connection c = createConnection();
+
         return null;
     }
+
+
 
     @Override
     public List<UserDTO> getUserList() throws DALException {
