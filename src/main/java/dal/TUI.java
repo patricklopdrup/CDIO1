e7c49b4272da7089c8d300a41a55408052cf87bd
Patrick <patrick.lopdrup@gmail.com>
2019-03-01 21:08:14 +0100

    lavet boolean isParsable

--- src/main/java/dal/TUI.java
+++ src/main/java/dal/TUI.java
@@ -367,4 +367,14 @@ public class TUI {
         }
         return true;
     }
+
+    private boolean isParsableInt(String input) {
+        boolean parsable = true;
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            parsable = false;
+        }
+        return parsable;
+    }
 }
